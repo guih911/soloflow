f2075876ef9985684e7a7d88dab2b7df40d80d76
guih911 <matheus.guih911@outlook.com>
2026-01-14 14:50:42 -0300

    postgress_database

--- soloflow-backend/prisma/add_table_columns.sql
+++
@@ -1,4 +0,0 @@
--- Adicionar campos para tipo TABLE no FormFieldVersion
-ALTER TABLE form_field_versions ADD COLUMN tableColumns TEXT;
-ALTER TABLE form_field_versions ADD COLUMN minRows INTEGER;
-ALTER TABLE form_field_versions ADD COLUMN maxRows INTEGER;
--- soloflow-backend/prisma/update_table_field.sql
+++
@@ -1,5 +0,0 @@
-UPDATE form_field_versions 
-SET tableColumns = '[{"name":"Nome","type":"TEXT","required":true},{"name":"Valor","type":"CURRENCY","required":true},{"name":"Data","type":"DATE","required":false}]', 
-    minRows = 1, 
-    maxRows = 10 
-WHERE name = 'teste' AND type = 'TABLE';
